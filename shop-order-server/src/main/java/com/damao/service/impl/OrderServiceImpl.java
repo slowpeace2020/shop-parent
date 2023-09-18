ffd31cd6e1e09e89cedc3cd86ff877280975ad9f
slowpeace2020 <slowpeace2020@gmail.com>
2023-09-18 17:12:32 -0400

    default LB robin

--- shop-order-server/src/main/java/com/damao/service/impl/OrderServiceImpl.java
+++ shop-order-server/src/main/java/com/damao/service/impl/OrderServiceImpl.java
@@ -7,7 +7,6 @@ import com.damao.domain.Product;
 import com.damao.service.IOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -29,10 +28,7 @@ public class OrderServiceImpl implements IOrderService {
     public Order createOrder(Long productId, Long userId) {
         log.info("receive order request, query product service to get product info by product id: {}",productId);
         //remote get  product service
-        ServiceInstance serviceInstance = discoveryClient.getInstances("product-service").get(0);
-        String host = serviceInstance.getHost();
-        int port = serviceInstance.getPort();
-        String url = "http://"+host+":"+port+"/product/"+productId;
+        String url = "http://product-service/product/"+productId;
 //        String url ="http://localhost:8081/product/"+productId;
         log.info("service url: {}",url);
         Product product = restTemplate.getForEntity(url,Product.class).getBody();
--- shop-product-server/src/main/java/com/damao/controller/ProductController.java
+++ shop-product-server/src/main/java/com/damao/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.damao.domain.Product;
 import com.damao.service.IProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,14 @@ public class ProductController {
     @Autowired
     private IProductService productService;
 
+    @Value("${server.port}")
+    private String port;
 
     @RequestMapping("/product/{pid}")
     public Product findByPid(@PathVariable("pid") Long pid){
         log.info("product query by pid: {} ",pid);
         Product product = productService.findByPid(pid);
+        product.setPname(product.getPname()+" "+port);
         log.info("product info: {}", JSON.toJSONString(product));
         return product;
     }
