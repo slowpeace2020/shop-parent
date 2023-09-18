fd20a0bb8e5fefd7ea060d4591b88250d321813e
slowpeace2020 <slowpeace2020@gmail.com>
2023-09-18 16:56:03 -0400

    remote service by discoveryClient

--- shop-order-server/src/main/java/com/damao/OrderServer.java
+++ shop-order-server/src/main/java/com/damao/OrderServer.java
@@ -2,6 +2,7 @@ package com.damao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ public class OrderServer {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate createRestTemplate(){
         return new RestTemplate();
     }
--- shop-order-server/src/main/java/com/damao/service/impl/OrderServiceImpl.java
+++ shop-order-server/src/main/java/com/damao/service/impl/OrderServiceImpl.java
@@ -7,6 +7,8 @@ import com.damao.domain.Product;
 import com.damao.service.IOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,12 +22,19 @@ public class OrderServiceImpl implements IOrderService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Override
     public Order createOrder(Long productId, Long userId) {
         log.info("receive order request, query product service to get product info by product id: {}",productId);
         //remote get  product service
-        String url ="http://localhost:8081/product/"+productId;
-
+        ServiceInstance serviceInstance = discoveryClient.getInstances("product-service").get(0);
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        String url = "http://"+host+":"+port+"/product/"+productId;
+//        String url ="http://localhost:8081/product/"+productId;
+        log.info("service url: {}",url);
         Product product = restTemplate.getForEntity(url,Product.class).getBody();
         log.info("query product service to get product info : {}", JSON.toJSONString(product));
         //create order
