8cd2d5b5b591ca2439f1b1fa7eaa0b7d8e53cd50
slowpeace2020 <slowpeace2020@gmail.com>
2023-09-30 17:20:40 -0400

    add gateway sentinel

---
+++ api-gateway/src/main/java/com/damao/config/GatewayConfiguration.java
@@ -0,0 +1,33 @@
+package com.damao.config;
+
+import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.BlockRequestHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class GatewayConfiguration {
+    @PostConstruct
+    public void initBlockHandlers() {
+        BlockRequestHandler blockRequestHandler = new BlockRequestHandler() {
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange
+                                                              serverWebExchange, Throwable throwable) {
+                Map map = new HashMap<>();
+                map.put("code", 0);
+                map.put("message", "接口被限流了");
+                return ServerResponse.status(HttpStatus.OK).
+                        contentType(MediaType.APPLICATION_JSON).
+                        body(BodyInserters.fromValue(map));
+            }
+        };
+    }
+}
